ca627ca6be35e8ac91a7801f55edf43d9c4c05b7
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2026-03-09 11:16:21 -0600

    Integración de LLM en grafos para agente que razonan

--- src/agents/simple.py
+++ src/agents/simple.py
@@ -1,28 +1,34 @@
 from langgraph.graph import StateGraph, MessagesState, START, END
 from langchain_core.messages import AIMessage, HumanMessage
+from langchain.chat_models import init_chat_model
+from langchain_openai import ChatOpenAI
+
+import random
+
+llm = init_chat_model("gemini-2.5-flash",model_provider="google_genai",temperature=0)
+gpt4o_llm = ChatOpenAI(model='gpt-4o', temperature=0)
+
 class State(MessagesState):
     customer_name: str
     my_age: int
 
 def node_1(state: State):
-    history = state["messages"]
+    new_state: State = {}
     if state.get("customer_name") is None:
-       return {
-           "customer_name": "John Doe"
-       }
+        new_state["customer_name"] = "John Doe"
     else:
-        ai_msg = AIMessage(content=f"Hello, {state.get('customer_name')}, how can I help you today?")
-        return {
-            "messages": [ai_msg]
-        }
+        new_state["my_age"] = random.randint(20, 30)
     
-    
-
+    history = state["messages"]
+    if history == []:
+        ai_message = gpt4o_llm.invoke('')
+    else:
+        ai_message = gpt4o_llm.invoke(history)
+    new_state["messages"] = [ai_message]
+    return new_state
 
 builder = StateGraph(State)
 builder.add_node("node_1",node_1)
-
 builder.add_edge(START,'node_1')
 builder.add_edge('node_1',END)
-
 agent = builder.compile()
