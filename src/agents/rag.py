4400b4aaf196afe2ae16ebadac1e9c0ecd53860b
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2026-03-16 17:57:08 -0600

    Respuestas estructuradas en LLMs para agentes

--- src/agents/rag.py
+++ src/agents/rag.py
@@ -2,6 +2,15 @@ from langgraph.graph import StateGraph, MessagesState, START, END
 from langchain_core.messages import AIMessage, HumanMessage
 from langchain.chat_models import init_chat_model
 from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+class ContactInfo(BaseModel):
+    """Contact information for a person"""
+    name: str = Field(description="The name of the person")
+    email: str = Field(description="The email of the address of the person")
+    phone: str = Field(description="The phone number of the person")
+    age: int = Field(description="The age of the person")
+
+llm_with_structured_output = ChatOpenAI(model='gpt-4o', temperature=0).with_structured_output(schema=ContactInfo)
 
 import random
 
@@ -17,21 +26,28 @@ llm = llm.bind_tools([file_search_tool])
 class State(MessagesState):
     customer_name: str
     my_age: int
+    phone: str
     
     
 def extractor(state: State):
-    return {}
+    customer_name = state.get("customer_name", None)
+    new_state: State = {}
+    if customer_name is None or len(state["messages"]) >= 10:
+        schema =llm_with_structured_output.invoke(state["messages"])
+        new_state["customer_name"] = schema.name
+        new_state["my_age"] = schema.age
+        new_state["phone"] = schema.phone
+    return new_state
 
 def conversation_node(state: State):
     new_state: State = {}
-    if state.get("customer_name") is None:
-        new_state["customer_name"] = "John Doe"
-    else:
-        new_state["my_age"] = random.randint(20, 30)
+
     
     history = state["messages"]
     last_message = history[-1].text
-    ai_message = llm.invoke(last_message)
+    customer_name = state.get('customer_name', 'John Doe')
+    system_message = f"You are a helpful assistant that can extract information from a conversation. The customer name is {customer_name}"
+    ai_message = llm.invoke([("system",system_message),("user",last_message)])
     text_content = ""
     for block in ai_message.content:
         if isinstance(block, dict) and "text" in block:
